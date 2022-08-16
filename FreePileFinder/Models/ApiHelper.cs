1b9b7ab818d8d3f17cf17dfd6185fd7532ac8dcb
Derrak Richard <derrak@gmail.com>
2022-08-16 16:14:31 -0700

    update CRUD
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- FreePileFinder/Models/ApiHelper.cs
+++ FreePileFinder/Models/ApiHelper.cs
@@ -5,12 +5,46 @@ namespace FreePileFinder.Models
 {
   class ApiHelper
   {
-    public static async Task<string> ApiCall(string apiKey)
+    public static async Task<string> GetAll()
     {
-      RestClient client = new RestClient("http://localhost:5000/api/");
+      RestClient client = new RestClient("http://localhost:5000/api");
       RestRequest request = new RestRequest($"piles", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
+    }
+
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"piles/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    public static async Task Post(string newPile)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"piles", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newPile);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Put(int id, string newPile)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"piles/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newPile);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"piles/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
     }
   }
 }
